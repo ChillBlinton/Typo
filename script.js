036bc331a9de4b52ea09760efca1f7c8a9b93348
CB <wtbatson@gmail.com>
2022-02-24 03:42:33 -0600

    setting display kermy

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 let $upKey = $("#keyboard-upper-container")
 let $lowKey = $("#keyboard-lower-container")
-let sentences = ['ten ate neite ate nee enet ite ate inet ent eate', 'Too ato too nOt enot one totA not anot tOO aNot', 'oat itain oat tain nate eate tea anne inant nean', 'itant eate anot eat nato inate eat anot tain eat', 'nee ene ate ite tent tiet ent ine ene ete ene ate'];
+let sentencesArr = ['ten ate neite ate nee enet ite ate inet ent eate', 'Too ato too nOt enot one totA not anot tOO aNot', 'oat itain oat tain nate eate tea anne inant nean', 'itant eate anot eat nato inate eat anot tain eat', 'nee ene ate ite tent tiet ent ine ene ete ene ate'];
+
+let sentences= 0;
+let letter= 0;
+let currentSentence = sentencesArr[sentences];
+let currentLetter = sentencesArr[letter]
 
 //toggling shift key 
 $(document).keydown(function (keyvent1) {
@@ -17,7 +22,7 @@ $(document).keydown(function (keyvent1) {
 });
 
 $(document).keypress(function (keyvent3) {
-    console.log(keyvent3.which)  //easier logout button press
+    console.log(keyvent3.which)  //easier logging button press
     let $key = $("#" + keyvent3.which);
     $($key).css("background-color", "aqua");
     $(document).keyup(function (keyvent3) {
@@ -25,18 +30,16 @@ $(document).keypress(function (keyvent3) {
     });
 });
 
-
-
-//check keys pressed
-// $("body").keyup(function (event) {
-//     console.log(event.keyCode);
-// });
-// $("body").keydown(function (event) {
-//     console.log(event.keyCode);
-// });
-// $("body").keypress(function (event) {
-//     console.log(event.keyCode);
-// });
+//sentence displays
+if (letter < currentSentence.length) {
+    letter + 1
+} else {
+    if (sentences < sentencesArr.length) {
+        sentences++
+        $("#sentence").text(currentSentence)
+        $("#feedback").empty();
+    }
+}
 
 
 //****DO NOT CAPS LOCK OR IT WILL TAKE YOU AN HOUR TO FIGURE OUT WHY THINGS ARENT LOGGING> JEEZ */
