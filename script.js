e0a2be6fe0a99076a4f8f87cd31e687a5e55a1cb
CB <wtbatson@gmail.com>
2022-02-24 01:14:31 -0600

    shift function & keystrokes

--- script.js
+++ script.js
@@ -1,3 +1,31 @@
 let $upKey = $("#keyboard-upper-container")
-let $downKMey = $("#keyboard-lower-container")
+let $lowKey = $("#keyboard-lower-container")
 
+//toggling shift key 
+$(document).keydown(function (event) {
+    if (event.which === 16) { //uopkeys
+        $($upKey).css("display", "block");
+        $($lowKey).css("display", "block");
+        $(document).keyup(function (event2) {
+            if (event2.which === 16) { //uopkeys
+                $($upKey).css("display", "block");
+                $($lowKey).css("display", "block");
+            }
+        });
+    }
+}
+
+)
+
+
+
+//check keys pressed & held
+$("body").keyup(function (event) {
+    console.log(event.keyCode);
+});
+$("body").keydown(function (event) {
+    console.log(event.keyCode);
+});
+$("body").keypress(function (event) {
+    console.log(event.keyCode);
+});
